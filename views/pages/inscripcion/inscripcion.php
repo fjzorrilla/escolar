fad7c5ea5856c265f9000f3631297966a29a3b0f
José Yzarra <jgyzarra@gmail.com>
2024-05-05 20:04:28 -0400

    Cambios en carpetas

--- views/pages/inscripcion/inscripcion.php
+++ views/pages/inscripcion/inscripcion.php
@@ -1,6 +1,27 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Dashboard</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard v1</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
 
-
-<div class="row">
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
           <div class="col-lg-6">
             <div class="card">
               <div class="card-body">
@@ -56,4 +77,9 @@
             </div>
           </div>
           <!-- /.col-md-6 -->
-        </div>
+</div>
+        <!-- /.row (main row) -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
